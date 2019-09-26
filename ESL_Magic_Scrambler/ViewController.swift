cfb248d74811763d3ed069898c4686d54f69b4f4
kssmith9 <kssmith9@ncsu.edu>
2019-09-26 20:28:17 +0700

    Add Test to VC.

--- ESL_Magic_Scrambler/ViewController.swift
+++ ESL_Magic_Scrambler/ViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
+    let scrambler = ScrambleMachine()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print(scrambler.scrambleWordString(theStringSays: "Test Time"))
     }
 
 
