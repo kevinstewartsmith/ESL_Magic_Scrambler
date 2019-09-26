7f92504c9d8b0f318f51e2cea460cac81fab3915
kssmith9 <kssmith9@ncsu.edu>
2019-09-26 16:30:04 +0700

    Initial Commit

---
+++ ESL_Magic_Scrambler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ESL_Magic_Scrambler
+//
+//  Created by Kevin Smith on 9/26/19.
+//  Copyright © 2019 Kevin Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
